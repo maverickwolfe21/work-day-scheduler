17dcf1b002d190373ff42a898cb8f3d0590d75a5
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2023-12-17 08:01:40 -0700

    added 9-5 with appropriate background colors

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 // in the html.
 $(function () {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+  const currentHour = dayjs().hour();
+  for (let i = 9; i < 16; i++) {
+    const workRow = $(`#hour-${i}`);
+    i < currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("past");
+    i === currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("present");
+    i > currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("future");
+  }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
