df8fac4e18f006f4450953c71875d720a6e8a62c
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2023-12-17 09:04:24 -0700

    added ability to save todos to local storage

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
-  const currentHour = dayjs().hour();
-  for (let i = 9; i < 16; i++) {
+  // updateUi();
+
+  const currentHour = dayjs().hour() - 9;
+  for (let i = 0; i < 9; i++) {
     const workRow = $(`#hour-${i}`);
     i < currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("past");
     i === currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("present");
     i > currentHour && workRow.removeClass("past").removeClass("present").removeClass("future").addClass("future");
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  $("button").click((e) => {
+    const parentId = Object.values($(e.currentTarget).parent())[0].id.substring(5);
+    const targetText = $(e.currentTarget).siblings("textarea").val();
+
+    let schedule = [];
+    const prevSchedule = JSON.parse(localStorage.getItem("schedule"));
+
+    if (prevSchedule) {
+      schedule = [...prevSchedule];
+      schedule[parentId].text = targetText.trim();
+      localStorage.setItem("schedule", JSON.stringify(schedule));
+    } else {
+      schedule = [
+        { time: "9AM", text: "" },
+        { time: "10AM", text: "" },
+        { time: "11AM", text: "" },
+        { time: "12PM", text: "" },
+        { time: "1PM", text: "" },
+        { time: "1PM", text: "" },
+        { time: "3PM", text: "" },
+        { time: "4PM", text: "" },
+        { time: "5PM", text: "" },
+      ];
+
+      schedule[parentId].text = targetText.trim();
+      localStorage.setItem("schedule", JSON.stringify(schedule));
+    }
+
+    // updateUi();
+  });
+
+  // function updateUi() {
+  //   const prevSchedule = JSON.parse(localStorage.getItem("schedule"));
+  //   if (prevSchedule) {
+  //     for (let i = 0; i < 9; i++) {
+  //       const workRow = $(`#hour-${i}`);
+  //       workRow.children(1).val(prevSchedule[i].text);
+  //     }
+  //   }
+  // }
 });
